90edc0c8cac3630aeb14b699ee7f3b4b5dce36ad
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2026-01-17 14:47:28 +0700

    feat:bff:bot_register

--- config/model.go
+++ config/model.go
@@ -1,7 +1,25 @@
 package config
 
 type Config struct {
-	Env  string `mapstructure:"environment" json:"environment"`
-	Port int    `mapstructure:"port" json:"port"`
-	Id   string `mapstructure:"id" json:"id"`
+	Env     string         `mapstructure:"environment" json:"environment"`
+	Port    int            `mapstructure:"port" json:"port"`
+	Id      string         `mapstructure:"id" json:"id"`
+	Binance BinanceService `mapstructure:"binance" json:"binance"`
+}
+
+type BinanceService struct {
+	PostionManagement BinancePostionManagement `mapstructure:"postion_management" json:"postion_management"`
+	Trade             BinanceTrade             `mapstructure:"trade" json:"trade"`
+}
+
+type BinancePostionManagement struct {
+	BaseUrl                string `mapstructure:"base_url" json:"base_url"`
+	UpdatePositionEndPoint string `mapstructure:"update_position_end_point" json:"update_position_end_point"`
+	DeletePositionEndPoint string `mapstructure:"delete_position_end_point" json:"delete_position_end_point"`
+	GetPositionEndPoint    string `mapstructure:"get_position_end_point" json:"get_position_end_point"`
+}
+
+type BinanceTrade struct {
+	BaseUrl          string `mapstructure:"base_url" json:"base_url"`
+	NewOrderEndPoint string `mapstructure:"new_order_end_point" json:"new_order_end_point"`
 }
